248f1f6f85e1bc8c61fd2b0ad014518dee535882
Ayaan38 <ayaanwarraich38@gmail.com>
2025-10-06 07:33:08 -0700

    Add files via upload

---
+++ Project 39.py
@@ -0,0 +1,5 @@
+attendance = input("Enter your attendance percentage as given on your report card")
+if attendance >=75 :
+    print("You are eligible for your exams")
+else :
+    print("Better luck next time")
